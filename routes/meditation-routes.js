65aae61c6d631e5f6b974464c383089ce87b7ce2
Yogi Spice <caseynnewman@yahoo.com>
2024-03-07 14:50:52 -0500

    created get, post, put and delete routes for meditations

--- routes/meditation-routes.js
+++ routes/meditation-routes.js
@@ -1,5 +1,79 @@
 const router = require('express').Router();
+const { Meditation, Instructor } = require('../models');
 
+// the `/meditations` endpoint
 
+router.get('/', async (req, res) => {
+    try {
+        const meditationData = await Meditation.findAll({
+            include: [{ model: Instructor }],
+        });
+        res.status(200).json(meditationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const meditationData = await Meditation.findByPk(req.params.id, {
+            include: [{ model: Instructor }],
+        });
+
+        if (!meditationData) {
+            res.status(404).json({ message: 'No meditation found with that ID!' });
+            return;
+        }
+
+        res.status(200).json(meditationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newMeditation = await Meditation.create({
+            title: req.body.title,
+        });
+        res.status(200).json(newMeditation);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const meditationData = await Meditation.update(
+            req.body, {
+            where: {
+                id: req.params.id,
+            }
+        }
+        );
+        res.status(204).json();
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const meditationData = await Meditation.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!meditationData) {
+            res.status(404).json({ message: 'No meditation found with that ID!' });
+            return;
+        }
+
+        res.status(200).json({ message: 'Meditation has been deleted!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
