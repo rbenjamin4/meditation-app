3f8fcd31fcb47632e2ee34924d8c3acafc47167c
Yogi Spice <caseynnewman@yahoo.com>
2024-03-06 21:12:44 -0500

    added foreign key to instructor and meditation models

--- models/instructor-model.js
+++ models/instructor-model.js
@@ -1,26 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Instructor extends Model {}
+class Instructor extends Model { }
 
 Instructor.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        meditationId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'meditation',
+                key: 'id'
+            },
+        }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    modelName: 'instructor',
-  }
+    {
+        sequelize,
+        underscored: true,
+        modelName: 'instructor',
+    }
 );
 
 module.exports = Instructor;
