095ddbc931bfc5aa93b2ce5b9af2220011d0a84e
Trent <trent.joyner.gmail.com>
2024-03-11 18:42:11 -0400

    added ! on line 62 of user-routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -59,7 +59,7 @@ router.delete('/:id', async (req, res)=> {
             id: req.params.id,
           },  
         });
-        if (userData) {
+        if (!userData) {
             res.status(404).json({ message: 'No account found with that id'});
             return;
         }
@@ -69,7 +69,5 @@ router.delete('/:id', async (req, res)=> {
         res.status(500).json(err);
     }
     });
-
-
-
+  
 module.exports = router;
