36a4847a732572f2995f2d2a631d11e482c5728f
Trent <trent.joyner.gmail.com>
2024-03-13 19:19:25 -0400

    branch with Juan

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const meditationRoutes = require('./meditation-routes');
 const instructorRoutes = require('./instructor-routes');
+const htmlRoutes = require('./htmlroutes')
 
 router.use('/users', userRoutes);
 router.use('/meditations', meditationRoutes);
 router.use('/instructors', instructorRoutes);
+router.use('/',htmlRoutes)
 
 module.exports = router;
