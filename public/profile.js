20ddd5173e67383f06a37024281ea54f5579fc4e
Heather Hannink <heatherhannink@Heathers-MacBook-Pro.local>
2024-03-13 19:46:13 -0400

    added user profile, created profile handlebars and profile js

---
+++ public/profile.js
@@ -0,0 +1,14 @@
+const profileFormHandler = async function(event) {
+    preventDefault();
+    
+    fName = document.querySelector('#fName-input');
+    lName = document.querySelector('lName-input');
+    uName = document.querySelector('uName-input');
+    email = document.querySelector('email-input');
+    goal = document.querySelector('goal-input');
+
+};
+
+document
+ .querySelector('#profile-form')
+ .addEventListener('save', profileFormHandler);
