42fa764357df00215fc75c9f5f81739391aa9cb7
Yogi Spice <caseynnewman@yahoo.com>
2024-03-13 21:56:37 -0400

    player routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Meditation, Instructor } = require('../models');
 
 router.get('/', (req, res) => {
     res.render('login') 
@@ -7,11 +8,24 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
    res.render('login') 
 })
-
-router.get('/home', (req, res) => {
-   res.render('home');
+//TODO: check if needs login
+router.get('/home', async (req, res) => {
+   try {
+      const meditationData = await Meditation.findAll({raw: true}, {
+          include: [{ model: Instructor }],
+      });
+      console.log(meditationData)
+      res.render('home', { data: meditationData });
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
+//get db info & pass
+//if time, when you click play btn it updates list
 
+router.get('/player/:id', (req, res) => {
+   res.render('player', { id:req.params.id });
+});
 
 
 
