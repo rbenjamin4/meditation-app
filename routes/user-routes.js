4592fd38bd4dcaf509ad3add1e634adcf79d2ec4
Trent <trent.joyner.gmail.com>
2024-03-16 22:13:25 -0400

    touch up to login css

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,140 +4,140 @@ const { User } = require('../models');
 
 //log in
 router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
-  
-      if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-  
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  // Logout
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
-
-router.delete('/:id', async (req, res)=> {
-    try {
-        const userData = await User.destroy({
-          where: {
-            id: req.params.id,
-          },  
-        });
-        if (!userData) {
-            res.status(404).json({ message: 'No account found with that id'});
-            return;
-        }
-
-        res.status(200).json({ message: 'Account has been deleted'});
-    } catch (err) {
-        res.status(500).json(err);
-      }
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
     });
 
-    //save user info
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
 
-    router.put('/profile/:id', (req, res) => {
-      const userInfo = req.params.id;
-      const updateInfo = req.body; 
-  
-      
-      User.update(updateInfo, { where: { id: userId } })
-          .then(() => {
-              res.status(200).json({ message: 'User Info successfully' });
-          })
-          .catch((error) => {
-              res.status(500).json({ error: 'Error updating user info' });
-          });
-  });
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    
-    router.put('/updatelistentime', async (req, res) => {
-      let userId = 1;
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData) {
+      res.status(404).json({ message: 'No account found with that id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Account has been deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//save user info
+
+router.put('/profile/:id', (req, res) => {
+  const userInfo = req.params.id;
+  const updateInfo = req.body;
 
 
-      let currentWeekNumber = getWeekNumber(new Date());
+  User.update(updateInfo, { where: { id: userId } })
+    .then(() => {
+      res.status(200).json({ message: 'User Info successfully' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error updating user info' });
+    });
+});
 
-      try {
-        const dbUserData = await User.findOne({
-          where: {
-            id: userId
-          },
-        });
-        console.log('User data week:', dbUserData.weekNumber, typeof dbUserData.weekNumber);
-        console.log('Current week:', currentWeekNumber);
 
-        if(dbUserData.weekNumber !== currentWeekNumber) {
+router.put('/updatelistentime', async (req, res) => {
+  let userId = 1;
 
-          await User.update(
-            {
-              weekNumber: currentWeekNumber,
-              timeListened: req.body.timeListened,
-            }, {
-              where: {
-                  id: userId
-              }
-            }
-          );
-          return res.status(204).json();
 
+  let currentWeekNumber = getWeekNumber(new Date());
+
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        id: userId
+      },
+    });
+    console.log('User data week:', dbUserData.weekNumber, typeof dbUserData.weekNumber);
+    console.log('Current week:', currentWeekNumber);
+
+    if (dbUserData.weekNumber !== currentWeekNumber) {
+
+      await User.update(
+        {
+          weekNumber: currentWeekNumber,
+          timeListened: req.body.timeListened,
+        }, {
+        where: {
+          id: userId
         }
-      } catch (err){
-        console.log('Reading user failed:', dbUserData);
       }
+      );
+      return res.status(204).json();
 
-      try {
-        console.log('SAving User...', req.body.timeListened)
-        const meditationData = await User.increment(
-          {
-            timeListened: req.body.timeListened
-          }, {
-            where: {
-                id: userId
-            }
-          }
-        );
-        console.log('SAVED USER.');
-        res.status(204).json();
+    }
+  } catch (err) {
+    console.log('Reading user failed:', dbUserData);
+  }
 
-      } catch (err) {
-        console.log('ERROR SAVING USER:' , err)
-        res.status(500).json(err);
+  try {
+    console.log('SAving User...', req.body.timeListened)
+    const meditationData = await User.increment(
+      {
+        timeListened: req.body.timeListened
+      }, {
+      where: {
+        id: userId
       }
+    }
+    );
+    console.log('SAVED USER.');
+    res.status(204).json();
+
+  } catch (err) {
+    console.log('ERROR SAVING USER:', err)
+    res.status(500).json(err);
+  }
 })
 
 
@@ -149,9 +149,9 @@ function getWeekNumber(d) {
   // Make Sunday's day number 7
   d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
   // Get first day of year
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
   // Calculate full weeks to nearest Thursday
-  var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+  var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
   // Return array of year and week number
   return weekNo;
 }
