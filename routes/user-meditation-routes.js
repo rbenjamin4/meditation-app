288cdcd2d231f03ada7a02b853bf9c948d95df6d
Yogi Spice <caseynnewman@yahoo.com>
2024-03-17 19:55:12 -0400

    working on user-meditation route for recent meditations

--- routes/user-meditation-routes.js
+++ routes/user-meditation-routes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const { UserMeditation, Meditation } = require('../models');
 
-// router.get('/home', async (req, res) => {
-//     try {
-//         const recentMeditations = await UserMeditation.findAll({raw: true,
-//             include: [{ model: Meditation }],
-//             order: [["date_time", "DESC"]],
-//             limit: 3,
-//         });
-//         res.render('home', { recent: recentMeditations });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+router.get('/home', async (req, res) => {
+    try {
+        const recentMeditations = await UserMeditation.findAll({
+            include: [{ model: Meditation }],
+            order: [["date_time", "DESC"]],
+            limit: 3,
+        });
+        res.render('home', { recent: recentMeditations });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
