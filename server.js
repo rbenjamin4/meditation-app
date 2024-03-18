4f029e94de8a61391ed7590ffe20cf622ff33552
Reed <reedbenjamin4@gmail.com>
2024-03-18 13:10:29 -0400

    Updated Port to 3001 for Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const hbs = create({ helpers });
 
