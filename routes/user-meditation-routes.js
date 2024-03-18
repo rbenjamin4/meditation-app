0f14fa1b4229504f300cf8b0c17699a8233714be
Yogi Spice <caseynnewman@yahoo.com>
2024-03-17 21:09:18 -0400

    adding comments for post route/recent meditations

--- routes/user-meditation-routes.js
+++ routes/user-meditation-routes.js
@@ -4,6 +4,7 @@ const { UserMeditation, Meditation } = require('../models');
 router.get('/home', async (req, res) => {
     try {
         const recentMeditations = await UserMeditation.findAll({
+            where: user.id === userId,
             include: [{ model: Meditation }],
             order: [["date_time", "DESC"]],
             limit: 3,
@@ -15,5 +16,7 @@ router.get('/home', async (req, res) => {
 });
 
 //post route for when they play to update time last played
+
+//meditation where userid
 
 module.exports = router;
