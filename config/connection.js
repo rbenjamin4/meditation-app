de93620f211811c9e684fd83842f2ae199729bca
Reed <reedbenjamin4@gmail.com>
2024-03-18 18:05:43 -0400

    new heroku db .env info

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
   }
 );
 
