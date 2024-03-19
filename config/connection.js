3ac7214bf516ab2814ba1b48eb7038695c47433b
Reed <reedbenjamin4@gmail.com>
2024-03-18 23:15:19 -0400

    Moved Heroku client code to connection.js and commented out. Added ssl: true to sequelize db info in connection.js

--- config/connection.js
+++ config/connection.js
@@ -16,10 +16,30 @@ sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: true
   }
 );
 
 }
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT Sessions, instructors, meditations, user_meditations, users FROM meditation_app.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -20,25 +20,6 @@ const sess = {
   }),
 };
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT Sessions, instructors, meditations, user_meditations, users FROM meditation_app.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 app.use(session(sess));
 
 app.use(express.static('public'));
