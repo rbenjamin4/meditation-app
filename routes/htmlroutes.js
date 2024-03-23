0cfec2303d836e2dc039f53dba885568e6bed6c7
Yogi Spice <caseynnewman@yahoo.com>
2024-03-23 15:27:36 -0400

    creating html route for profile page

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,50 +2,47 @@ const router = require('express').Router();
 const { Meditation, Instructor, UserMeditation, User } = require('../models');
 const withAuth = require('../utils/auth');
 
- router.get('/login', (req, res) => {
-    res.render('login') 
- })
+router.get('/login', (req, res) => {
+    res.render('login')
+})
 
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/home');
-      return;
-    } else if (!req.session.loggedIn){
+        res.redirect('/home');
+        return;
+    } else if (!req.session.loggedIn) {
         res.redirect('/login');
     }
-  
-    
-  });
+});
 
 router.get('/player', async (req, res) => {
-   res.render('player')
+    res.render('player')
 })
 
-//TODO: check if needs login
 router.get('/home', withAuth, async (req, res) => {
-   try {
-      let meditationData = await Meditation.findAll({
-          include: [{ model: Instructor }],
-         //  order: [["users.user_meditation.date_time", "DESC"]],
-          limit: 3,
-      });
-      meditationData = meditationData.map((value) => {
-        return value.get({plain: true});
-      })
-      let userData = await User.findByPk(req.session.userId, {
-        attributes: {
-            exclude: ['username', 'email', 'password']
-        }
-      });
-      userData = userData.get({plain: true});
-      console.log(userData)
-      res.render('home', {
-        meditations: meditationData,
-        user: userData,
-    });
-  } catch (err) {
-      res.status(500).json(err);
-  }
+    try {
+        let meditationData = await Meditation.findAll({
+            include: [{ model: Instructor }],
+            // order: [["users.user_meditation.date_time", "DESC"]],
+            limit: 3,
+        });
+        meditationData = meditationData.map((value) => {
+            return value.get({ plain: true });
+        })
+        let userData = await User.findByPk(req.session.userId, {
+            attributes: {
+                exclude: ['username', 'email', 'password']
+            }
+        });
+        userData = userData.get({ plain: true });
+        console.log(userData)
+        res.render('home', {
+            meditations: meditationData,
+            user: userData,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/user', async (req, res) => {
@@ -56,50 +53,44 @@ router.get('/user', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-  });
+});
 
-//get db info & pass
-//if time, when you click play btn it updates list
-
-// router.get('/home', async (req, res) => {
-//    try {
-//        const recentMeditations = await UserMeditation.findAll({raw: true,
-//            include: [{ model: Meditation }],
-//            order: [["date_time", "DESC"]],
-//            limit: 3,
-//        });
-//        res.render('home', { recent: recentMeditations });
-//    } catch (err) {
-//        res.status(500).json(err);
-//    }
-// });
+router.get('/profile', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        console.log(userData)
+        res.render('profile', { user: userData });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/api/meditations/:id', async (req, res) => {
-   try {
-      const meditationData = await Meditation.findByPk(req.params.id, {
-          include: [{ model: Instructor }],
-      });
-      console.log(meditationData)
-      res.render('player', { id:req.params.id, data });
-  } catch (err) {
-      res.status(500).json(err);
-  }
+    try {
+        const meditationData = await Meditation.findByPk(req.params.id, {
+            include: [{ model: Instructor }],
+        });
+        console.log(meditationData)
+        res.render('player', { id: req.params.id, data });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/trackList', async (req, res) => {
-   try {
-      const trackListData = await Meditation.findAll({
-          attributes: ['fileName']
-      });
-      console.log(trackListData)
-      res.status(200).json(trackListData)
-  } catch (err) {
-      res.status(500).json(err);
-  }
+    try {
+        const trackListData = await Meditation.findAll({
+            attributes: ['fileName']
+        });
+        console.log(trackListData)
+        res.status(200).json(trackListData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 router.get('/profile', (req, res) => {
-   res.render('profile') 
+    res.render('profile')
 })
 
 
